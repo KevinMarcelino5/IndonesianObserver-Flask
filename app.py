965ee82411d41640cee4e1970b996aa046441054
KevinMarcelino5 <mfkmusung@gmail.com>
2023-04-26 14:32:09 +0800

    hashed password n password field

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
@@ -30,6 +31,20 @@ class Users(db.Model):
     email = db.Column(db.String(200), nullable=False, unique=True)
     favorite_food = db.Column(db.String(200))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Password stuff
+    password_hash = db.Column(db.String(128))
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute !')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
         
     # create string
     def __repr__(self):
@@ -65,6 +80,8 @@ class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_food = StringField("Favorite Food")
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='password must match')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 # update database record
@@ -106,13 +123,19 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user =  Users(name=form.name.data, email=form.email.data, favorite_food=form.favorite_food.data)
+            # hash password
+            hashed_pw= generate_password_hash(form.password_hash.data, 'sha256')
+            user =  Users(name=form.name.data,
+                          email=form.email.data,
+                          favorite_food=form.favorite_food.data,
+                          password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
         form.favorite_food.data = ''
+        form.password_hash.data = ''
         flash('User Added successfully')
     our_users = Users.query.order_by(Users.date_added)
     
